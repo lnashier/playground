d6d24fd11b7e9bbadbda90bfc0fb5054d1cc2d48
lnashier <17622088+lnashier@users.noreply.github.com>
2024-03-21 14:35:13 -0700

    +comments

--- slices/accumulate.go
+++ slices/accumulate.go
@@ -4,6 +4,8 @@ import (
 	"golang.org/x/exp/maps"
 )
 
+// Accumulate accumulates elements in the slice based on a specified ID function and accumulation function.
+// It combines elements with the same ID using the accumulation function.
 func Accumulate[T any](ts []T, accumulateFn func(T, T) T, idFn func(T) any) []T {
 	if len(ts) < 2 {
 		return ts
@@ -11,6 +13,8 @@ func Accumulate[T any](ts []T, accumulateFn func(T, T) T, idFn func(T) any) []T 
 	accumulated := make(map[any]T)
 	for _, t := range ts {
 		id := idFn(t)
+		// If an element with the same ID exists in the map,
+		// apply the accumulation function to combine them.
 		if ot, ok := accumulated[id]; ok {
 			t = accumulateFn(t, ot)
 		}
